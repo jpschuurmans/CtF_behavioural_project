f644b6e742de0703893623aba1996d6139fd5531
Jolien Schuurmans <jolienschuurmans@gmail.com>
2022-09-12 12:13:48 +0200

    lets start with this initial push

---
+++ CtFBackwardMasking.py
@@ -0,0 +1,460 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Wed Jan 26 14:41:32 2022
+
+Experiment code:
+Psychophysical coarse-to-fine backward masking. 
+
+@author: jschuurmans
+"""
+
+
+#%% ===========================================================================
+# paths
+
+base_path = 'C:/Users/Adminuser/Documents/03_SFmasking/MainExp/'
+
+stim_path = f'{base_path}stimuli/'
+mask_path = f'{base_path}masks/'
+back_path = f'{base_path}background/'
+data_path = f'{base_path}data/'
+asfx='.bmp'
+
+
+#%% ===========================================================================
+# imports
+
+import os
+os.chdir(base_path)
+import numpy as np
+import itertools
+from psychopy import visual, event, core, gui, data, monitors
+from PIL import Image
+import csv
+from functions_ctfbackwardmasking import *
+
+
+#%% ===========================================================================
+# different conditions in experiment
+
+spatialfrequencies = ['LSF','HSF']
+durations = ['50','100','150']
+#different_conditions = list(itertools.product(spatialfrequencies,durations))
+
+# for the paradigm we need more trial contitions
+matching = ['same','diff']
+staircases = ['1','2']
+
+
+#%% ===========================================================================
+# monitor setup + subject info
+
+exp_name = 'Coarse-to-fine backward masking'
+exp_info = {
+        '1. subject' : 'sub-',
+        '2. gender' : ('female','male'),
+        '3. age' : '',
+        '4. screenwidth(cm)' : '59',
+        '5. screenwidth(pix)' : '1920',
+        '6. screenhight(pix)' : '1200', # '1080',
+        '7. refreshrate(hz)' : '60', #ViewPixx - Change this (120)
+        '8. screendistance' : '57'
+        }
+dlg = gui.DlgFromDict(dictionary=exp_info, title=exp_name)
+    
+
+# If 'Cancel' is pressed, quit
+if dlg.OK == False:
+    core.quit()
+        
+# Get date and time
+exp_info['date'] = data.getDateStr()
+exp_info['exp_name'] = exp_name
+
+mon = monitors.Monitor('Dell') #Pulls out photometer calibration settings by name.  
+# Change this if you are using the ViewPixx (you should find the proper name in PsychoPy Monitor Center)
+mon.setDistance(exp_info['8. screendistance'])
+mon.setWidth(exp_info['4. screenwidth(cm)']) # Cm width
+framerate = exp_info['7. refreshrate(hz)'] 
+scrsize = (float(exp_info['5. screenwidth(pix)']),float(exp_info['6. screenhight(pix)'] ))
+framelength = 1000/(float(framerate))
+mon.setSizePix(scrsize)
+
+
+#%% ===========================================================================
+# Timing
+
+fix_dur = 500 # fixation before trial input in ms
+int_dur = 500 # time between fixation and trial  
+mask_dur = 200 # duration mask
+
+nframe = num_frames(fix_dur,int_dur,mask_dur,framelength)
+
+
+#%% ===========================================================================
+## load and organise images into a block and trial lists
+
+# get names of all images folders. Naming should be:
+# stimulus: ID01_IM01.bmp / mask: BG01_ID01_IM01_LSF.bmp / background: BG01.bmp
+# returns self.path and self.stim (list of names)
+stim = Stimuli(stim_path) 
+
+# returns list with unique dr of IDs or IMs self.unique_nr
+stim.getuniquenr('ID')
+stim.getuniquenr('IM')
+
+# returns self.same_list / self.diff_list / self.maxnr_trials 
+stim.list_of_combinations()
+
+
+
+# creates self.sf, self.dur, self.match, self.stair and an empty self.trial_list
+alltrials = Ordertrials(stim,spatialfrequencies,durations,matching,staircases)
+
+# Creating and shuffle the trials with balanced number of conditions
+alltrials.trial_list(framelength)
+alltrials.shuffle_trials()
+
+
+
+# Making sure all big blocks have equal number of each condition
+# 8 big blocks -> each have 6 miniblocks with 16 trials each
+# each miniblock has different condition (LSF50,LSF100,LSF150,HSF50,HSF100,HSF150)
+n_bigblock = 8
+miniblock_per_bigblock = len(alltrials.conditionlist)
+trials_per_block = 16 # that is 8 per staircase
+
+# creates self.blocks['block-1']['HSF_50']['stair-1'][0] 
+# in this case: 8 blocks, 6 conditions, 2 staircases, 16 trials
+alltrials.make_miniblocks(n_bigblock,miniblock_per_bigblock,trials_per_block)
+    
+
+
+#%% ===========================================================================
+# make Psi Staircase for all conditions (x2)
+
+# nTrials is trials PER staircase
+nTrials = int((trials_per_block/len(alltrials.stair))*n_bigblock)
+signal_start = 30 # signal of blending (e.g. signal = 30, alpha = 70)
+signal_end = 60
+steps = 30
+
+# initialize a staircase for each condition
+alltrials.prepare_staircare(nTrials,signal_start,signal_end,steps)
+
+
+
+
+#%% ===========================================================================
+# open file csv log file to write the data
+
+if not os.path.isdir(data_path):
+    os.makedirs(data_path)
+
+data_fname = exp_info['1. subject'] + '_' + exp_info['2. gender'] +  exp_info['3. age'] + '_' + exp_info['date'] + '.csv'
+data_fname = os.path.join(data_path, data_fname)
+f = open(data_fname,'w',encoding='UTF8', newline='')
+
+header_names = list(alltrials.blocks['block-1']['HSF_100']['stair-1'][0].keys())
+
+writer=csv.DictWriter(f, fieldnames=header_names)
+writer.writeheader()
+
+                           
+#%% ===========================================================================
+# prepare clock
+change_clock = core.Clock()
+rt_clock = core.Clock()
+
+# Open a window
+win = visual.Window(monitor = mon, size = scrsize, color ='grey', units ='pix', fullscr = True)
+
+
+textpage = visual.TextStim(win, height=32, font="Palatino Linotype", alignHoriz='center' )
+
+instructiontexts = {
+    1 : """In this experiment, you will see two faces displayed
+    one after another.\n Your task is to report whether the two 
+    faces have the same identity.""",
+    2 : """If both pictures are of the SAME person, press "S",\n
+    if they  are different people, press "L"\n\n 
+    Press SPACE key to continue."""
+    }
+
+for text in instructiontexts:
+    instructions = textpage
+    instructions.text = instructiontexts[text]
+    instructions.draw()
+    win.flip()
+
+    keys = event.waitKeys(keyList=['space','escape'])#core.wait(.1)
+
+
+# Hide cursor when window is open
+win.mouseVisible=False
+
+
+# for debugging: win.close()
+
+
+#%% =============================================================================
+#window setup
+#cra
+instruc01 = 'Welcome!\nHopefully you are comfortable and relaxed.\n\nDuring this experiment you will see faces flashed on the screen.\nThe only thing you should do\nis press a button when the colour changes.\n\nPress a button to continue.\n(1 -> buttonbox key)'
+instruc01 = visual.TextStim(win, color='black', height=32, text=instruc01)
+instruc02 = 'The experiment is about to start!\n\n Waiting for the scanner trigger.. (s)'
+instruc02 = visual.TextStim(win, color='black',height=32,text=instruc02)
+
+#create fixation cross
+fix1=visual.Line(win,start=(-stimSize,-stimSize),end=(stimSize, stimSize),
+                 pos=(0.0, 0.0),lineWidth=1.0,lineColor='black',units='pix')
+fix2=visual.Line(win,start=(-stimSize,stimSize),end=(stimSize, -stimSize),
+                 pos=(0.0, 0.0),lineWidth=1.0,lineColor='black',units='pix')
+
+
+
+
+instruc01.draw()
+win.flip()
+while not '1' in event.getKeys():
+    core.wait(0.1)
+
+instruc02.draw()
+win.flip()
+while not 's' in event.getKeys():
+    core.wait(0.1)
+    
+
+# for debugging: win.close()
+
+#%% ===========================================================================
+# draw fixation Cross
+fixation = visual.ShapeStim(win, 
+    vertices=((0, -0.3), (0, 0.3), (0,0), (-0.3,0), (0.3, 0)),
+    lineWidth=3,
+    closeShape=False,
+    lineColor="black"
+    )
+
+# prepare bitmaps for presenting images
+stimsize = [7.05,8.38]
+bitmap1 = visual.ImageStim(win, size=stimsize, interpolate=True) 
+bitmap2 = visual.ImageStim(win, size=stimsize, interpolate=True) 
+bitmap_mask = visual.ImageStim(win, size=stimsize, interpolate=True)
+
+
+
+
+# We draw the instruction text
+instruc01 = visual.TextStim(win, color='black', height=32, text=instruc01)
+instructions = visual.TextStim(
+    win,
+    height=32,
+    font="Palatino Linotype",
+    alignHoriz='center'
+    )
+
+    
+instructions.text = """
+In this experiment, you will see two faces displayed one after another.\n 
+Your task is to report whether the two faces have the same identity."""
+instructions.draw()
+
+instructions2=instructions
+instructions2.text = """
+If both pictures are of the SAME person, press "S",\n
+if they different people, press "L"\n\n 
+
+Press SPACE key to continue.
+"""
+
+instructions2.pos=[0,0]
+instructions2.draw()
+
+#show instruction text
+win.flip()
+keys = event.waitKeys(keyList=['space','escape'])#core.wait(.1)
+
+
+# for debugging: win.close()
+
+#%% ===========================================================================
+
+
+trialinfo = alltrials.blocks['block-1']['HSF_100']['stair-1'][0]
+visibility = alltrials.blocks['block-1']['HSF_100']['stair-1_Psi']
+
+draw = loadimage(base_path, trialinfo, visibility)
+
+############################# blending mask is missing !
+
+
+
+
+
+# Start experiment
+i=0
+block_no = -1
+for block in final_blocks:
+    block_no += 1
+    if block_no > 0:
+        block_break(block_no)
+    for trial in block:
+        if trial['ori']=='up':
+            ori=180
+            if trial['cond'][0] == 's' :
+                while sameup.xCurrent == None:
+                    pass # hang in this loop until the psi calculation has finished
+                visibility = sameup.xCurrent
+            elif trial['cond'][0] == 'd':
+                while diffup.xCurrent == None:
+                    pass
+                visibility = diffup.xCurrent
+            elif trial['cond'][0] == 'i':
+                while isoup.xCurrent == None:
+                    pass
+                visibility = isoup.xCurrent
+        else:
+            ori=0
+            if trial['cond'][0] == 's':
+                while sameinv.xCurrent == None:
+                    pass # hang in this loop until the psi calculation has finished
+                visibility = sameinv.xCurrent
+            elif trial['cond'][0] == 'd':
+                while diffinv.xCurrent == None:
+                    pass
+                visibility = diffinv.xCurrent
+            elif trial['cond'][0] == 'i':
+                while isoinv.xCurrent == None:
+                    pass
+                visibility = isoinv.xCurrent
+            
+        im1=np.array(Image.open(os.path.join(stim_path,trial['im1name'])))
+        im2=np.array(Image.open(os.path.join(stim_path,trial['im2name'])))
+        mask=(np.array(Image.open(os.path.join(mask_path,trial['mask']))))/256
+        im1us=occlude(im1,visibility)
+        im1=(im1us-127.5)/127.5
+        im2us=occlude(im2,visibility)
+        im2=(im2us-127.5)/127.5
+        mask_occluded=occlude(mask, visibility)
+        maskfinal=(mask_occluded-127.5)/127.5 # .5 is the midvalue so you'd do (x-.5)/.5
+        
+        bitmap1.setOri(ori)
+        bitmap2.setOri(ori)
+        bitmap_mask.setOri(ori)
+        
+        # you don't need this
+        if trial['ori']== 'up':
+            eyeleveling=-1.18 
+            # eyeleveling=-1.38
+        else:
+            eyeleveling=1.18
+            # eyeleveling=1.38
+        
+        bitmap1.pos=(0,eyeleveling) #142+284/2 (5.1 is equal to 142 pixels, then we add half of the horizontal size (7/2) because pos. takes the center to the defined location.)
+        bitmap2.pos=(0,eyeleveling)
+        bitmap_mask.pos=(0,eyeleveling)
+        
+        
+        
+        
+         
+        bitmap1.setImage(im1)
+        bitmap2.setImage(im2)
+        bitmap_mask.setImage(maskfinal)
+        
+        for nFrames in range(FixFrame): # 600 ms.
+                fixation.draw()
+                win.flip()
+                
+        for nFrames in range(IntFrame):  # 500 ms
+                win.flip()
+            
+        for nFrames in range(ImFrame): # 500 ms
+                bitmap1.draw()
+                win.flip()
+                    
+        for nFrames in range(MaskFrame): # 200 ms
+               bitmap_mask.draw() # We don't have a mask right now
+               win.flip()
+               
+               
+        bitmap2.draw()
+        win.flip()
+        change_clock.reset()
+        rt_clock.reset()
+                                        
+        # Wait until a response, or until time limit.
+        # keys = event.waitKeys(maxWait=timelimit, keyList=['s','l', 'escape'])
+                 
+        keys = event.waitKeys(keyList=['s','l','escape','p'])     
+        if keys:
+            rt = rt_clock.getTime()
+            # fixation.clearTextures()
+            
+        bitmap1.clearTextures()
+        bitmap2.clearTextures()
+        win.flip()
+        
+        if not keys:
+            keys = event.waitKeys(keyList=['s','l','escape'])
+            rt = rt_clock.getTime()
+                           
+        acc = 0
+        if keys:
+            if 'escape' in keys:
+                f.close()
+                win.close()
+                # exTrials.saveAsWideText('Exp_full' + '.csv', delim=',')
+                win.mouseVisible=True
+                break
+            elif 's' in keys and (trial['cond'] == 'ss' or trial['cond'] == 'ds' or trial['cond'] == 'isos'): # is same
+                acc = 1
+            elif 'l' in keys and (trial['cond'] == 'sd' or trial['cond'] == 'dd' or trial['cond'] == 'isod'): # is different
+                acc = 1         
+                
+        trial['acc']=acc
+        trial['rt']=rt
+        trial['contrast']=visibility
+        trial['trialno']=i
+        # exTrials.addData('acc', acc)
+        # exTrials.addData('rt', rt)
+        # exTrials.addData('visib',visibility)
+        i+=1
+
+        # Update staircase        
+        
+        ''' elegantere shii dan een if else loop
+        obj = {
+            's': samup.addDaga(acc)
+            'd':  diffup.addData(acc)
+            }
+        
+        obj['s']
+        '''
+        
+        if ori==180:
+            if trial['cond'][0] == 's' :
+                sameup.addData(acc)
+            elif trial['cond'][0] == 'd':
+                diffup.addData(acc)
+            elif trial['cond'][0] == 'i':
+                isoup.addData(acc)
+        else:
+            if trial['cond'][0] == 's':
+                sameinv.addData(acc)
+            elif trial['cond'][0] == 'd':
+                diffinv.addData(acc)
+            elif trial['cond'][0] == 'i':
+                isoinv.addData(acc)
+        writer.writerow(trial)
+
+f.close()
+# exTrials.saveAsWideText('Exp_full' + '.csv', delim=',')
+win.close()
+win.mouseVisible=True
+
+
+#%% =============================================================================
---
+++ functions_ctfbackwardmasking.py
@@ -0,0 +1,296 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  9 20:29:50 2022
+
+Functions for CtF backward masking experiment
+
+@author: jschuurmans
+
+"""
+
+#%% =============================================================================
+# imports
+import os
+import numpy as np
+from PIL import Image # this is used to manipulate images and do the alpha-blending
+import itertools
+import copy
+import random
+import numpy.random as rnd
+import PsiMarginal 
+
+#%% =============================================================================
+# functions
+
+def nframes(time_in_ms, framelength):
+     return int(time_in_ms/framelength)
+
+
+def num_frames(fix_dur, int_dur, mask_dur,framelength):
+    nframe = {}
+      
+    nframe['fix'] = nframes(fix_dur,framelength)
+    nframe['int'] = nframes(int_dur,framelength)
+    nframe['mask'] = nframes(mask_dur,framelength)
+    return nframe
+
+
+def makePsi(nTrials,signal_start,signal_end,steps): # start_thresh is signal strength in percentage
+# Image visibility ranges between 1.5 and 40, logarithmically, 40 possibilities.
+    staircase = PsiMarginal.Psi(stimRange=np.geomspace(signal_start,signal_end,steps,endpoint=True),
+            Pfunction='Weibull', nTrials=nTrials,
+            threshold=np.geomspace(signal_start,signal_end,steps, endpoint=True), 
+            thresholdPrior=('normal',5,5), slope=np.geomspace(0.5, 20, 50, endpoint=True),
+            guessRate=0.5, slopePrior=('gamma',3,6), lapseRate=0.05, lapsePrior=('beta',2,20), marginalize=True)
+    return staircase
+# sigma is slope
+
+
+
+def loadimage(base_path,trialinfo,visibility):
+    # loading and occluding image ready for drawing
+    stimuli = {'stim1' : 'stimuli',
+               'stim2' : 'stimuli',
+               'mask' : 'masks',
+               'background' : 'background'}
+    draw = {}
+    all_loaded = {}
+    for curr in stimuli:
+        stim_path = f'{base_path}{stimuli[curr]}/'
+        loaded_image = np.array(Image.open(os.path.join(stim_path,trialinfo[curr])))
+        all_loaded[curr] = loaded_image
+    for im in range(1,3):
+        occluded_image = occlude(all_loaded[f'stim{im}'],all_loaded['background'],visibility)
+        #occluded_image = (occluded_image-127.5)/127.5 # do I also have to do this? ############
+        draw[f'stim{im}'] = occluded_image
+    draw['mask'] = all_loaded['mask']
+    return draw
+
+
+
+def occlude(image, back, visibility):
+    fullAlpha = 255 # highest image pixel value
+    signal = visibility.xCurrent
+    # create Alpha channel array for the image, fill it with 255s
+    imHeight, imWidth = image.shape[:]
+    srcA = np.full([imHeight,imWidth], fullAlpha)
+    # same for backrgound
+    backHeight, backWidth = back.shape[:]
+    backA = np.full([backHeight, backWidth], fullAlpha)
+    
+    # reduce Alpha transparency of image / background
+    srcA[:, :] = fullAlpha -((100-signal)/100*fullAlpha)
+    backA[:, :] = ((100-signal)/100*fullAlpha)
+    # blend them into a single image
+    blend=(image*(srcA/fullAlpha))+(back*(backA/fullAlpha))
+    # visualise: Image.fromarray(blend.astype('uint8'))
+    return blend
+
+
+def block_break(block_no):
+    timer=3
+    # timer=1
+    blocktext = visual.TextStim(
+                    win=win,
+                    height=.5,
+                    font="Palatino Linotype",
+                    alignHoriz='center'
+                    )   
+    timertext = visual.TextStim(win=win,
+            height=.5, 
+            pos=[0,-5],
+            font="Palatino Linotype",
+            alignHoriz = 'center')
+    if block_no % 6 == 0:
+        timer=20
+        # timer=0
+    blocktext.text="""Please take a short rest before the next block.
+    You can press "SPACE" to start again 
+    after """ + str(timer) + """ seconds when you are ready.
+
+    Block: """ + str(block_no) + """/48"""
+    for time in range(timer):
+        timer-=1
+        blocktext.draw()
+        timertext.text=""":""" + str(timer)
+        timertext.draw()
+        core.wait(1)
+        win.flip()
+        if timer == 0:
+            timertext.text="""READY"""
+            blocktext.draw()
+            timertext.draw()
+            win.flip()
+    keys = event.waitKeys(keyList=['space','escape'])
+    if 'escape' in keys:
+        win.close()
+        f.close()
+    win.flip()
+    core.wait(2)
+
+
+
+#%% =============================================================================
+# classes
+
+
+class Stimuli:
+    # Stimuli() saves self.path and self.stim (list of names)
+    def __init__(self, image_path):
+        # get names of all images folders
+        self.path = image_path
+        self.list = os.listdir(os.path.join(image_path))
+        self.unique_nr = {}
+        
+    def loadimages(self):
+        # opens all stimuli and adds them to an array self.im_array
+        stim_list = []
+        for im in range(0, len(self.list)):
+            stim_list.append(Image.open(os.path.join(self.path,self.list[im]), mode="r"))
+        im_array = np.array([np.array(im) for im in stim_list])
+        self.im_array = im_array
+
+    def getuniquenr(self,whichtype): 
+        # function makes a list of unique numbers
+        # if name of images are ID01_IM01.bmp
+        # then 'whichtype' should be either 'ID' or 'IM'
+        type_list = []
+        for im in self.list:
+            type_list.append(int(im.rsplit(whichtype)[1][0:2]))
+        unique_nr = np.unique(np.array(type_list))
+        # returns list with unique dr of IDs or IMs self.unique_nr
+        self.unique_nr[whichtype] = unique_nr
+        
+    def list_of_combinations(self):
+        # returns self.same_list / self.diff_list / self.maxnr_trials 
+        same_list = []
+        diff_list = []
+        for idx,uniqueid in enumerate(self.unique_nr['ID']):
+            nr_unique_images = len(self.unique_nr['IM'])
+            list_curr_id = self.list[(idx*nr_unique_images):nr_unique_images+(idx*nr_unique_images)]
+            comb_same_list = list(itertools.combinations(list_curr_id, r=2))
+            same_list.append(comb_same_list)
+            
+            list_no_curr_id = copy.deepcopy(self.list)
+            del list_no_curr_id[(idx*nr_unique_images):nr_unique_images+(idx*nr_unique_images)] # delete current id from list
+            comb_diff_list = list(itertools.product(list_curr_id,list_no_curr_id))
+                        #select as many combinatoins as the same list has..
+            comb_diff_list = random.choices(comb_diff_list,k=len(comb_same_list))
+            
+            diff_list.append(comb_diff_list)
+
+        
+        self.same_list = same_list
+        self.diff_list = diff_list
+        self.maxnr_trials = np.shape(same_list)[0]*np.shape(same_list)[1] # nr identities times nr images
+        # same_list[0] = all "same" combinations of the first identity
+        # same_list[0][0] = first possible "same" combination
+
+
+
+# =============================================================================
+
+
+
+class Ordertrials(object):
+    
+    def __init__(self,stim,spatialfrequencies,durations,matching,staircases):
+        self.sf = spatialfrequencies
+        self.dur = durations
+        self.match = matching
+        self.stair = staircases
+        self.stim = stim
+
+    # Creating the trials with balanced number of conditions
+    def trial_list(self,framelength):
+        different_trials = ["_".join(items) for items in itertools.product(self.sf ,self.dur,self.match,self.stair)]
+        self.conditionlist = ["_".join(items) for items in itertools.product(self.sf ,self.dur)]
+        
+        # make this a dictionary
+        self.trial_list = {}
+        for key in different_trials:
+            self.trial_list[key] = []
+            
+        matchingcond = {'same' : self.stim.same_list,
+                        'diff' : self.stim.diff_list}
+        
+        for SF in self.sf:
+                for dur in self.dur:
+                    ImFrame = int(int(dur)/framelength)
+                    for idx,whichid in enumerate(self.stim.unique_nr['ID']): # loop through ID's
+                        for trialtype in self.match: # same or different trial                        
+                            triallist = matchingcond[trialtype]
+                            for cmb,combi_per_id in enumerate(triallist[idx]): #all possible combinations per ID
+                                for stair in self.stair: #two staircases per condition
+                                    trial_name = f'{SF}_{dur}_{trialtype}_{stair}' # trial type name
+                                    #the two backgrounds will alternate depening on the staircase nr
+                                    back = f'BG0{str(int(stair)+1)}.bmp'
+                                    num1 = round(random.random())
+                                    if num1 == 0: # randomise which image is presented as image1
+                                        num2 = 1 # making sure image 2 is the other image... :)
+                                    else:
+                                        num2 = 0
+                                    # trial_list will be a dictionary with 24 keys
+                                    # the conditions are SF*duration (2*3 = 6 in the example)
+                                    # this is doubled to have the same/diff conditions for the matching task (2*6 = 12)
+                                    # this is doubled because there are 2 staircases per condition (2*12 = 24)
+                                    self.trial_list[trial_name].append({'stim1' : combi_per_id[num1],
+                                                                 'stim2' : combi_per_id[num2],
+                                                                 'duration' : dur,
+                                                                 'nframes' : ImFrame,
+                                                                 'SF' : SF,
+                                                                 'mask' : f'{back[:-4]}_{combi_per_id[num1][:-4]}_{SF}.bmp',
+                                                                 'background' : back,
+                                                                 'staircasenr': stair,
+                                                                 'rt' : 0,
+                                                                 'acc' : 0,
+                                                                 'contrast' : 0,
+                                                                 'trialno' : 0})  
+                                    
+    def shuffle_trials(self):
+        # here we shuffle trials from each condition within the condition block
+        for cond in self.trial_list.keys(): 
+            rnd.shuffle(self.trial_list[f"{cond}"])
+          
+            
+    def make_miniblocks(self,n_bigblock,miniblock_per_bigblock,trials_per_block):
+        # to make all the blocks ready
+        # big blocks containing smaller blocks (small block = one condition per block)
+        # small blocks contain trails. half same, half different trials.
+        self.n_bigblock = n_bigblock
+        self.miniblock_per_bigblock = miniblock_per_bigblock
+        self.trials_per_block = trials_per_block
+        self.blocks = {}    
+    
+        mini_blocks = {}
+        stair = {}
+        tempblock=[]
+        blocklist = {}
+        
+        for bblock in range(n_bigblock):
+            name = f'block-{bblock+1}'
+            for idx,cond in enumerate(self.conditionlist):
+                for stairnr in self.stair:
+                # for every mini block, grab 8 unique trials.. and go on to the next trial
+                    for trial in range(int(trials_per_block/len(self.stair))): # 8 trials of one condition per block
+                        #tempblock[f'trial-{trial}_diff']= self.trial_list[f'{cond}_diff_{stairnr}'][trial*(idx+1)]
+                        #tempblock[f'trial-{trial}_same'] = self.trial_list[f'{cond}_same_{stairnr}'][trial*(idx+1)]
+                        tempblock.append(self.trial_list[f'{cond}_diff_{stairnr}'][trial*(idx+1)])
+                        tempblock.append(self.trial_list[f'{cond}_same_{stairnr}'][trial*(idx+1)])
+                    rnd.shuffle(tempblock)
+                    stair[f'stair-{stairnr}'] = tempblock
+                    tempblock=[]
+                mini_blocks[cond] = stair
+            blocklist[name] = mini_blocks
+        self.blocks = blocklist
+
+    def prepare_staircare(self,nTrials,signal_start,signal_end,steps):
+        for block in self.blocks:
+            for cond in self.blocks[block]:
+                for stairnr in self.stair:
+                    self.blocks[block][cond][f'stair-{stairnr}_Psi'] = makePsi(nTrials,signal_start,signal_end,steps)
+        
+
+ # =============================================================================       
+  
+ 
